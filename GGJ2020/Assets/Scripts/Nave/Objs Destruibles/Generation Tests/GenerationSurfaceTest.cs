9c9d303063d568f73dca3bf13422f00e668e29b7
Santiago Morales Lab <santiago.morales.alv@gmail.com>
2020-02-20 14:55:23 -0500

    recuperacion primeras lineas

--- GGJ2020/Assets/Scripts/Nave/Objs Destruibles/Generation Tests/GenerationSurfaceTest.cs
+++ GGJ2020/Assets/Scripts/Nave/Objs Destruibles/Generation Tests/GenerationSurfaceTest.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class GenerationSurfaceTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject pf_Test;
+
+    [SerializeField]
+    private Transform[] points;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log(GenerateNewTest());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject GenerateNewTest()
     {
-        
+        Vector3 verticePoint = Vector3.zero;
+        //Vector3 verticePoint = mesh.mesh.vertices[Random.Range(0, mesh.mesh.vertexCount)];
+
+        GameObject test = null;
+
+        test = Instantiate(pf_Test, SelectRandomPoint(), Quaternion.identity);
+        VRTK.VRTK_SDKManager.instance.loadedSetup
+        return test;
     }
+    private Vector3 SelectRandomPoint()
+    {
+        int num = Random.Range(0, points.Length);
+        return points[num].position;
+    }
+
 }
+
