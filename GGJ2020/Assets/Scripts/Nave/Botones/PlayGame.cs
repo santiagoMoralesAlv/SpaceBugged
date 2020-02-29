c8f41f6da72972092b29ef963229e87a93f8a2b7
Santiago Morales Lab <santiago.morales.alv@gmail.com>
2020-02-28 23:10:50 -0500

    Prog_AudioImplementation

---
+++ GGJ2020/Assets/Scripts/Nave/Botones/PlayGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGame : ActiveButton
+{
+    override public void Use()
+    {
+        base.Use();
+        if (!ControlGame.Instance.InGame)
+        {
+            ControlGame.Instance.StartGame();
+        }
+    }
+}
