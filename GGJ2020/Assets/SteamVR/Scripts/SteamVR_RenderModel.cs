6f9d27caf12d89a904423ae244284b2a43e168d5
Santiago Morales Lab <santiago.morales.alv@gmail.com>
2020-03-06 16:08:37 -0500

    Anim_Aduption and FutureMaterial

--- GGJ2020/Assets/SteamVR/Scripts/SteamVR_RenderModel.cs
+++ GGJ2020/Assets/SteamVR/Scripts/SteamVR_RenderModel.cs
@@ -240,7 +240,8 @@ public class SteamVR_RenderModel : MonoBehaviour
 
 						// Check the cache first.
 						var material = materials[renderModel.diffuseTextureId] as Material;
-						if (material == null || material.mainTexture == null)
+                        //material.mainTexture == null
+                        if (material == null )
 						{
 							var pDiffuseTexture = System.IntPtr.Zero;
 
