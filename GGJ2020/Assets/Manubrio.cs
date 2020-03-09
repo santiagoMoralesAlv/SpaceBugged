bc30721c9c8b70a4d4c3e10e42ce8e385bc17ea0
Santiago Morales Alvarez <santiago.morales.alv@gmail.com>
2020-03-09 06:09:51 -0500

    avance de Prog_Manubrio

---
+++ GGJ2020/Assets/Manubrio.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manubrio : MonoBehaviour
+{
+    private Animator animator;
+
+    [SerializeField]
+    private Transform tf;
+
+    [SerializeField]
+    private float distance;
+
+    private bool showing;
+
+    private void Awake()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (showing)
+        {
+            if (Vector3.Distance(HeadsetReference.Instance.HeadsetTf.position, tf.position) > distance)
+            {
+                showing = !showing;
+                UpdateAnimator();
+            }
+        }
+        else {
+            if (Vector3.Distance(HeadsetReference.Instance.HeadsetTf.position, tf.position) < distance)
+            {
+                showing = !showing;
+                UpdateAnimator();
+            }
+        }
+    }
+
+    private void UpdateAnimator() {
+        animator.SetBool("Show", showing);
+    }
+}
